744959790343b4de36a3a08f47148f83ff736d8e
izumi <izumigcs@gmail.com>
2022-10-18 13:35:08 +0900

    こっくりさん修正(小文字,濁点→無しとみなす判定追加)

--- Assets/izumi/Kokkurisan/Kokkurisan.cs
+++ Assets/izumi/Kokkurisan/Kokkurisan.cs
@@ -132,7 +132,7 @@ public class Kokkurisan : MonoBehaviour
                 //// 文字の場合
                 //else
                 //{
-                    int answerNo = (int)answerChara[i] - 12354;
+                    int answerNo = (int)answerChara[i] - 12353;
 
                     if (charList[answerNo] != null)
                     {
@@ -144,25 +144,7 @@ public class Kokkurisan : MonoBehaviour
             // 正解した文字の場合は〇で文字を囲む
             if (markObjList.Count == 0)
             {
-                char[] clearChara = clearStr.ToCharArray();
-                missNum = 0;
-                for (int i = 0; i < clearChara.Length; i++)
-                {
-                    // 回答した文字列の中で正解の文字を含んでいた場合
-                    if (answerStr.Contains(clearChara[i]))
-                    {
-                        int clearNo = (int)clearChara[i] - 12354;
-                        GameObject markObj = Instantiate(markClear.gameObject, charList[clearNo].transform);
-                        markObj.transform.localPosition = new Vector3(-100, -25, 0);
-                        markObj.SetActive(true);
-                        markObjList.Add(markObj);
-                    }
-                    else
-                    {
-                        // 間違い文字数を追加
-                        missNum++;
-                    }
-                }
+                ClearJudge(answerStr, clearStr);
             }
 
             // 間違えた文字数を表示
@@ -238,6 +220,42 @@ public class Kokkurisan : MonoBehaviour
                 }
             }
             markObjList.Clear();
+        }
+    }
+
+    // 正解文字を〇で囲む処理
+    void ClearJudge(string answerStr,string clearStr)
+    {
+        char[] clearChara = clearStr.ToCharArray();
+        missNum = 0;
+        for (int i = 0; i < clearChara.Length; i++)
+        {
+            bool isClear = false;
+            // 回答した文字列の中で正解の文字を含んでいた場合
+            for(int j = 0; j < answerStr.Length; j++)
+            {
+                // 表示文字リストで同じ文字の場合、正解と判定
+                if(charList[i] == charList[j])
+                {
+                    isClear = true;
+                }
+            }
+
+            // 正解だったら(文字を含むか)
+            if (isClear)
+            {
+                int clearNo = (int)clearChara[i] - 12353;
+                GameObject markObj = Instantiate(markClear.gameObject, charList[clearNo].transform);
+                markObj.transform.localPosition = new Vector3(-100, -25, 0);
+                markObj.SetActive(true);
+                markObjList.Add(markObj);
+            }
+            // 不正解だったら(文字を含んでなかった)
+            else
+            {
+                // 間違い文字数を追加
+                missNum++;
+            }
         }
     }
 }
