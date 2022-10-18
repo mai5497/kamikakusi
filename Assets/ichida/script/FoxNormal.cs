49d1daca244b6b35e4ae4a3867baff06b6f7ae84
itida <maimai130806@gmail.com>
2022-10-18 19:19:05 +0900

    狐演出途中

---
+++ Assets/ichida/script/FoxNormal.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxNormal : MonoBehaviour
+{
+    private SpriteRenderer sr;  // 狐のスプライトレンダラー
+    private float Alpha;    // 狐のアルファ値
+
+    private const float appearTime = 1.5f;  // アルファ値が上がりきるまでの時間
+
+
+    private bool isByakko;
+
+    private bool byakko;
+
+    private FoxByakko fox;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        Alpha = 0.0f;
+        sr.color = new Color(1, 1, 1, Alpha);
+
+        fox = GameObject.Find("FoxByakko").GetComponent<FoxByakko>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        byakko = fox.GetByakko_delete();
+
+        if (byakko) {
+            Alpha += Time.deltaTime / appearTime;
+            sr.color = new Color(1, 1, 1, Alpha);
+        }
+    }
+}
