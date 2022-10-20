ff6699fbe22e6bc7fdeb569d81dd12c9bf636643
itida <maimai130806@gmail.com>
2022-10-20 19:01:47 +0900

    タイトル追加

---
+++ Assets/ichida/script/TitleManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //----- ƒTƒEƒ“ƒh -----
+        for (int i = 0; i < SoundData.TitleAudioList.Length; ++i) {
+            SoundData.TitleAudioList[i] = gameObject.AddComponent<AudioSource>();
+        }
+        SoundManager2.Play(SoundData.eBGM.BGM_GAME01, SoundData.TitleAudioList);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
