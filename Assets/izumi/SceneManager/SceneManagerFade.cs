909ecfe962d166197d60f7dbac7c672c108613be
izumi <izumigcs@gmail.com>
2022-10-21 12:40:18 +0900

    こっくりさんの片方エフェクト、次のステージへシーン遷移 (未テスト)

--- Assets/izumi/SceneManager/SceneManagerFade.cs
+++ Assets/izumi/SceneManager/SceneManagerFade.cs
@@ -44,12 +44,31 @@ public static class SceneManagerFade
     // 次のステージシーンの読み込み
     public static void LoadSceneNextStage()
     {
-        // ステージ番号が最大では無ければ、
+        bool isNextWorld = false;
+        // ステージ番号が最大は無ければ、
         if (SceneManagerData.nowStageNo < SceneManagerData.mainSceneStrArray.GetLength(1) - 1)
         {
-            if (SceneManagerData.mainSceneStrArray[SceneManagerData.nowWorldNo, SceneManagerData.nowStageNo + 1] == null)
+            // 次のステージがnullで無ければ
+            if (SceneManagerData.mainSceneStrArray[SceneManagerData.nowWorldNo, SceneManagerData.nowStageNo + 1] != null)
             {
-                
+                LoadSceneMain(SceneManagerData.nowWorldNo, SceneManagerData.nowStageNo + 1);
+            }
+            else
+            {
+                isNextWorld = true;
+            }
+        }
+        else
+        {
+            isNextWorld = true;
+        }
+
+        if (isNextWorld)
+        {
+            // ワールド番号が最大で無ければ次のワールドシーンへ
+            if (SceneManagerData.nowWorldNo < SceneManagerData.mainSceneStrArray.GetLength(0) - 1)
+            {
+                LoadSceneMain(SceneManagerData.nowWorldNo + 1, 0);
             }
         }
     }
